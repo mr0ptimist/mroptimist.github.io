6bcf47390c6d5afe764a521c3540524ed3f4cf03
MrOptimist <1428059826@qq.com>
2026-05-04 21:31:56 +0800

    其他修改

--- bat/serve_启动预览.bat
+++ bat/serve_启动预览.bat
@@ -1,5 +1,11 @@
 @echo off
 cd /d "%~dp0.."
+
+:: Relaunch in Windows Terminal if not already
+if "%WT_SESSION%"=="" (
+    wt -w 0 nt -d "%CD%" cmd /c "%~f0" %*
+    exit /b
+)
+
 taskkill /F /IM hugo.exe >nul 2>&1
 hugo server -D
-pause
